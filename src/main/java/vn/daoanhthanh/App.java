86ac651b7b7676ec7d423f01397cbc70e2853954
Edward <daoanhthanh.work@gmail.com>
2021-10-30 10:29:44 +0700

    init

---
+++ src/main/java/vn/daoanhthanh/App.java
@@ -0,0 +1,15 @@
+package vn.daoanhthanh;
+
+import vn.daoanhthanh.GUI.MainScreen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainScreen screen = new MainScreen();
+        screen.show();
+    }
+}
---
+++ src/main/java/vn/daoanhthanh/network/WritableGUI.java
@@ -0,0 +1,7 @@
+package vn.daoanhthanh.network;
+
+public interface WritableGUI {
+
+    void write(String s);
+
+}
